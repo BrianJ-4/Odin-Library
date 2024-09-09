a8a0de095ca7d87d0854c3fab27fa39987e01975
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-08 20:30:07 -0700

    Added implementation of addBookToLibrary

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const myLibrary = [];
+const displayedBooks = [];
+
+const addBookButton = document.getElementById("addBookButton");
+addBookButton.addEventListener('click', (event) => {
+    addBookToLibrary(bookOne);
+});
+
+const bookGrid = document.getElementById("book-grid");
 
 function Book(title, author, pages, read)
 {
@@ -11,4 +19,41 @@ function Book(title, author, pages, read)
 function addBookToLibrary(book)
 {
     myLibrary.push(book);
+    
+    const newBook = document.createElement("div");
+    newBook.className = "book-card";
+
+    const titleDisplay = document.createElement("div");
+    const authorDisplay = document.createElement("div");
+    const pagesDisplay = document.createElement("div");
+    const readButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    titleDisplay.id = "title";
+    authorDisplay.id = "author";
+    pagesDisplay.id = "pages";
+    readButton.id = "read-button";
+    removeButton.id = "remove-button"
+
+    titleDisplay.innerText = book.title;
+    authorDisplay.innerText = book.author;
+    pagesDisplay.innerText = book.pages + " Pages";
+    removeButton.innerText = "Remove";
+
+    if (book.read)
+    {
+        readButton.className = "read";
+        readButton.innerText = "Read";
+    }
+    else
+    {
+        readButton.className = "unread";
+        readButton.innerText = "Unread";
+    }
+
+    newBook.append(titleDisplay, authorDisplay, pagesDisplay, readButton, removeButton);
+
+    bookGrid.append(newBook);
 }
+
+let bookOne = new Book("Test Book", "John Doe", 234, true);
