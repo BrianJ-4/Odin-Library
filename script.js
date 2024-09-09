63733f77461548006cc866aa487929a688e3fe8a
BrianJ-4 <brian.joshua03@gmail.com>
2024-09-08 19:42:37 -0700

    Initial creation of files

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;  
+}
+
+function addBookToLibrary(book)
+{
+    myLibrary.push(book);
+}
